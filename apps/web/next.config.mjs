fe1e854e96b3b203c8c1a93366133c0c4cd90368
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-03-24 10:07:01 -0300

    :truck: (tsconfig) rename typescript config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@repo/ui', "@repo/server", "@repo/schemas", "@repo/typescript-config"]
+  transpilePackages: ['@repo/ui', "@repo/server", "@repo/schemas", "@repo/tsconfig"]
 }
 
 export default nextConfig
